de39149e510b08befcabe16d5e7047ff26b3e356
Krystal <krystalsimm01@me.com>
2020-08-27 19:33:15 +0100

    Added files for tests

---
+++ src/test/java/com/qa/trainers/TrainersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.trainers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
