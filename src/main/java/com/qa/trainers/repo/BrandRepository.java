0d0f32d11a01b7e32478604925e7baec337f45cd
Krystal <krystalsimm01@me.com>
2020-08-28 13:05:58 +0100

    Created brand repository

---
+++ src/main/java/com/qa/trainers/repo/BrandRepository.java
@@ -0,0 +1,9 @@
+package com.qa.trainers.repo;
+
+import com.qa.trainers.domain.Brand;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BrandRepository extends JpaRepository<Brand, Long> {
+}
