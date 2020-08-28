3f88847891b23f6b6124ca563043219de48410af
Krystal <krystalsimm01@me.com>
2020-08-28 14:26:24 +0100

    Added comments to code

---
+++ src/main/java/com/qa/trainers/rest/ShoeController.java
@@ -0,0 +1,44 @@
+package com.qa.trainers.rest;
+
+import com.qa.trainers.domain.Shoe;
+import com.qa.trainers.service.ShoeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+public class ShoeController {
+
+    private final ShoeService shoeService;
+
+    @Autowired
+    public ShoeController(ShoeService shoeService) {
+        this.shoeService = shoeService;
+    }
+
+    @GetMapping("/")
+    public List<Shoe> getAllShoes(){
+        return this.shoeService.seeAllShoes(); //Read functionality
+    }
+
+    @PostMapping("/addShoe")
+    public Shoe addShoe(@RequestBody Shoe shoe){ // Add/create functionality
+        return this.shoeService.addShoe(shoe);
+    }
+
+    @DeleteMapping("/delete/{shoeId}")
+    public Boolean deleteShoe(@PathVariable Long shoeId){ // Delete functionality
+        return this.shoeService.deleteShoeById(shoeId);
+    }
+
+    @GetMapping("/getShoeById/{shoeId}")
+    public Shoe getShoeById(@PathVariable Long shoeId){
+        return this.shoeService.findShoeById(shoeId);
+    }
+
+    @PutMapping("/updateShoeSize/{shoeId}")
+    public Shoe updateShoeSize(@PathVariable Long shoeId, @RequestBody Shoe shoe){ // Update functionality
+        return this.shoeService.updateShoe(shoeId, shoe);
+    }
+
+}
