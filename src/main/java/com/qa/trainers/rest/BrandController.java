a78e01852c70c4b9e5d98b1a2c49263b144cb040
Krystal <krystalsimm01@me.com>
2020-09-01 00:50:52 +0100

    Updated brand controller to return DTOs

--- src/main/java/com/qa/trainers/rest/BrandController.java
+++ src/main/java/com/qa/trainers/rest/BrandController.java
@@ -2,7 +2,10 @@ package com.qa.trainers.rest;
 
 import com.qa.trainers.domain.Brand;
 import com.qa.trainers.service.BrandService;
+import com.qa.trainers.dto.BrandDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,28 +21,28 @@ public class BrandController {
     }
 
     @GetMapping("/getAllBrands")
-    public List<Brand> getAllBrands(){
-        return this.brandService.seeAllBrands(); //Read functionality
-    }
+    public ResponseEntity<List<BrandDTO>> getAllBrands(){
+        return ResponseEntity.ok(this.brandService.seeAllBrands());
+    } //Read functionality
 
     @PostMapping("/addBrand")
-    public Brand addBrand(@RequestBody Brand brand){ // Create/add functionality
-        return this.brandService.addBrand(brand);
-    }
+    public ResponseEntity<BrandDTO> addBrand(@RequestBody Brand brand){
+        return new ResponseEntity<>(this.brandService.addBrand(brand), HttpStatus.CREATED);
+    } //add (create) functionality. Line 30 - explicit type argument, can be replaced with <>
 
     @DeleteMapping("/deleteBrand/{brandId}")
     public Boolean deleteBrand(@PathVariable Long brandId){ // Delete functionality
         return this.brandService.deleteBrandById(brandId);
-    }
+    }//delete functionality
 
     @GetMapping("/getBrandById/{brandId}")
-    public Brand getBrandById(@PathVariable Long brandId){
-        return this.brandService.findBrandById(brandId);
-    }
+    public ResponseEntity<BrandDTO> getBrandById(@PathVariable Long brandId){
+        return ResponseEntity.ok(this.brandService.findBrandById(brandId));
+    } //get brand by ID
 
     @PutMapping("/updateBrand/{brandId}")
-    public Brand updateBrand(@PathVariable Long brandId, @RequestBody Brand brand){ // Update functionality
-        return this.brandService.updateBrand(brandId, brand);
+    public ResponseEntity<BrandDTO> updateBrand(@PathVariable Long brandId, @RequestBody Brand brand){
+        return ResponseEntity.ok(this.brandService.updateBrand(brandId, brand));
     }
 
 }
