7c80eea6fec123288adec22c6a73c6bc60bad632
Krystal <krystalsimm01@me.com>
2020-09-01 00:50:31 +0100

    Updated brand service to return DTOs

--- src/main/java/com/qa/trainers/service/BrandService.java
+++ src/main/java/com/qa/trainers/service/BrandService.java
@@ -1,39 +1,49 @@
 package com.qa.trainers.service;
 
 import com.qa.trainers.domain.Brand;
+import com.qa.trainers.dto.BrandDTO;
 import com.qa.trainers.exceptions.BrandNotFoundException;
 import com.qa.trainers.repo.BrandRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class BrandService {
 
     private final BrandRepository brandRepo;
 
+    private final ModelMapper mapper;
+
     @Autowired
-    public BrandService(BrandRepository brandRepo) {
+    public BrandService(BrandRepository brandRepo, ModelMapper mapper) {
         this.brandRepo = brandRepo;
+        this.mapper = mapper;
     }
 
-    public List<Brand> seeAllBrands(){
-        return this.brandRepo.findAll();
-    } //show all brands
-
-    public Brand addBrand(Brand brand){
-        return this.brandRepo.save(brand);
-    } // add brand
-
-    public Brand findBrandById(Long brandId){
-        return this.brandRepo.findById(brandId).orElseThrow(BrandNotFoundException::new); // find brand by ID
+    private BrandDTO mapToDTO(Brand brand){
+        return this.mapper.map(brand, BrandDTO.class);
     }
 
-    public Brand updateBrand(Long brandId, Brand brand){ // update brand name
-        Brand brandUpdate = findBrandById(brandId);
+    public List<BrandDTO> seeAllBrands(){
+        return this.brandRepo.findAll().stream().map(this::mapToDTO).collect(Collectors.toList());
+    } //show (read) all brands
+
+    public BrandDTO addBrand(Brand brand){
+        return this.mapToDTO(this.brandRepo.save(brand));
+    } // add (create) brand
+
+    public BrandDTO findBrandById(Long brandId){
+        return this.mapToDTO(brandRepo.findById(brandId).orElseThrow(BrandNotFoundException::new));
+    } // find brand by ID
+
+    public BrandDTO updateBrand(Long brandId, Brand brand){
+        Brand brandUpdate = this.brandRepo.findById(brandId).orElseThrow(BrandNotFoundException::new);
         brandUpdate.setBrandName(brand.getBrandName());
-        return this.brandRepo.save(brandUpdate);
+        return this.mapToDTO(this.brandRepo.save(brandUpdate));
     }
 
     public Boolean deleteBrandById(Long brandId){
