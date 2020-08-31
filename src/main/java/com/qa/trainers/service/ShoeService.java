118b73574ce970034d386596bae05def43aa39ea
Krystal <krystalsimm01@me.com>
2020-09-01 00:11:05 +0100

    Updated shoe service to return DTOs

--- src/main/java/com/qa/trainers/service/ShoeService.java
+++ src/main/java/com/qa/trainers/service/ShoeService.java
@@ -1,40 +1,51 @@
 package com.qa.trainers.service;
 
 import com.qa.trainers.domain.Shoe;
+import com.qa.trainers.dto.ShoeDTO;
 import com.qa.trainers.exceptions.ShoeNotFoundException;
 import com.qa.trainers.repo.ShoeRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ShoeService {
 
     private final ShoeRepository shoeRepo;
 
+    private final ModelMapper mapper;
+
     @Autowired
-    public ShoeService(ShoeRepository shoeRepo){
+    public ShoeService(ShoeRepository shoeRepo, ModelMapper mapper){
         this.shoeRepo = shoeRepo;
+        this.mapper = mapper;
     }
 
-    public List<Shoe> seeAllShoes(){
-        return this.shoeRepo.findAll();
-    } //show all shoes
+    private ShoeDTO mapToDTO(Shoe shoe){
+           return this.mapper.map(shoe, ShoeDTO.class);
+        }
 
-    public Shoe addShoe(Shoe shoe){
-        return this.shoeRepo.save(shoe);
-    } // add shoe
+    public List<ShoeDTO> seeAllShoes() {
+        return this.shoeRepo.findAll().stream().map(this::mapToDTO).collect(Collectors.toList());
+    } //see (read) all shoes
 
-    public Shoe findShoeById(Long shoeId){
-        return this.shoeRepo.findById(shoeId).orElseThrow(ShoeNotFoundException::new); // find shoe by ID
-    }
 
-    public Shoe updateShoe(Long shoeId, Shoe shoe){ // update shoe size
-        Shoe update = findShoeById(shoeId);
+    public ShoeDTO addShoe(Shoe shoe){
+        return this.mapToDTO(this.shoeRepo.save(shoe));
+    } // add (create) shoe
+
+    public ShoeDTO findShoeById(Long shoeId){
+        return this.mapToDTO(this.shoeRepo.findById(shoeId).orElseThrow(ShoeNotFoundException::new));
+    } //find shoe by ID
+
+    public ShoeDTO updateShoeSize(Long shoeId, Shoe shoe){
+        Shoe update = this.shoeRepo.findById(shoeId).orElseThrow(ShoeNotFoundException::new);
         update.setShoeSize(shoe.getShoeSize());
-        return this.shoeRepo.save(update);
-    }
+        return this.mapToDTO(this.shoeRepo.save(shoe));
+    } //update shoe size
 
     public Boolean deleteShoeById(Long shoeId){
         if(!this.shoeRepo.existsById(shoeId)){
@@ -42,6 +53,6 @@ public class ShoeService {
         }
         this.shoeRepo.deleteById(shoeId);
         return this.shoeRepo.existsById(shoeId); // delete shoe
-    }
+    } // delete shoe
 
 }
