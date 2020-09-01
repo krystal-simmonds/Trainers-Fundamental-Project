36912fafbe2952083501ff6c440f43487d10dfda
Krystal <krystalsimm01@me.com>
2020-09-01 23:25:09 +0100

    Created user repository

--- src/main/java/com/qa/trainers/repo/UserRepository.java
+++ src/main/java/com/qa/trainers/repo/UserRepository.java
@@ -3,5 +3,5 @@ package com.qa.trainers.repo;
 import com.qa.trainers.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
