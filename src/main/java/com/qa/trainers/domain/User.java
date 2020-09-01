edca93022f6fc87c92f9bd55cde45abb314520d9
Krystal <krystalsimm01@me.com>
2020-09-01 23:03:55 +0100

    Created User class

---
+++ src/main/java/com/qa/trainers/domain/User.java
@@ -0,0 +1,64 @@
+package com.qa.trainers.domain;
+
+public class User {
+
+    private Long userId;
+
+    private String fName;
+
+    private String lName;
+
+    private String email;
+
+    private String password;
+
+    public User(){
+    }
+
+    public User(String fName, String lName, String email, String password) {
+        this.fName = fName;
+        this.lName = lName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
