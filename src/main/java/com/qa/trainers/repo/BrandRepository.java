7e3c4d6585d0640c0dd89ef9523770adfeb88450
Krystal <krystalsimm01@me.com>
2020-09-03 01:07:26 +0100

    Added functionality to find brand by name

--- src/main/java/com/qa/trainers/repo/BrandRepository.java
+++ src/main/java/com/qa/trainers/repo/BrandRepository.java
@@ -4,6 +4,9 @@ import com.qa.trainers.domain.Brand;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BrandRepository extends JpaRepository<Brand, Long> {
+    Optional<Object> findByBrandName(String brandName);
 }
