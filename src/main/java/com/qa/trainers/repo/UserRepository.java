bd4c13d72eace778bec435a23e8d90474f074c1f
krystal-simmonds <69357541+krystal-simmonds@users.noreply.github.com>
2020-09-04 12:18:56 +0100

    Delete UserRepository.java

--- src/main/java/com/qa/trainers/repo/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.qa.trainers.repo;
-
-import com.qa.trainers.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
