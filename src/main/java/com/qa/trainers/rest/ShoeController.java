0dcfe697a7e9a001ede9414df725a3a17d2f4717
Krystal <krystalsimm01@me.com>
2020-09-17 08:13:37 +0100

    commented out unused import statements in controller

--- src/main/java/com/qa/trainers/rest/ShoeController.java
+++ src/main/java/com/qa/trainers/rest/ShoeController.java
@@ -3,13 +3,13 @@ package com.qa.trainers.rest;
 import com.qa.trainers.domain.Shoe;
 import com.qa.trainers.dto.ShoeDTO;
 import com.qa.trainers.service.ShoeService;
-import org.hibernate.boot.jaxb.Origin;
+//import org.hibernate.boot.jaxb.Origin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
+//import javax.websocket.server.PathParam;
 import java.util.List;
 
 @RestController
