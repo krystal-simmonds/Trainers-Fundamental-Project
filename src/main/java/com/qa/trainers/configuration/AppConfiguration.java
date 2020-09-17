b46febf95b74924f5959de4bff161a211ed2390c
Krystal <krystalsimm01@me.com>
2020-09-17 12:48:15 +0100

    commented out unused import statement

--- src/main/java/com/qa/trainers/configuration/AppConfiguration.java
+++ src/main/java/com/qa/trainers/configuration/AppConfiguration.java
@@ -3,7 +3,7 @@ package com.qa.trainers.configuration;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.ui.ModelMap;
+//import org.springframework.ui.ModelMap;
 
 @Configuration
 public class AppConfiguration {
